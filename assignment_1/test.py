6f726d56d69df5a22474ccf9c83a51d8fe2937e8
Daniele Brazzolotto <s111071@n-62-24-13.(none)>
2013-01-09 21:53:54 +0100

    test.py

---
+++ assignment_1/test.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import re
+import math
+from subprocess import Popen, PIPE
+import datetime
+
+memory_approx =   [(x) for x in [16, 32, 64, 128, 256 , 512, 1024, 2048, 4096]] #, 4096, 8192, 16384]] 
+implementation = ["nat","nmk","nkm","knm","kmn","mnk","mkn"] # "blk"] (when ready)
+times = 1
+ 
+def main():
+	ns = [(int(math.ceil(math.sqrt(x*1024/(3*8))))) for x in memory_approx]
+	print ns
+	now = datetime.datetime.now()
+	filename = "results_%s.dat" % (now.strftime("%Y-%m-%d_%H-%M"))
+	print "Tests running..."
+	for i in range(len(memory_approx)):
+		fi = open(filename,"a+")
+		fi.write(str(memory_approx[i]))
+		fi.close()
+		for test in implementation:
+			print "Testing matmult_%s(), %d time(s), %d kB memory." % (test,times, memory_approx[i])
+			process = Popen(("./matmult_f.studio %s %d %d %d" % (test,ns[i],ns[i],ns[i]+1)).split() ,stdout= PIPE,bufsize=-1) 
+			(stdout,stderr) = process.communicate()
+			process.wait()
+			st = re.sub(' +',' ', stdout).split(" ")[1:]
+			print "Result: " 
+			if stdout == "":
+				fi = open(filename,"a+")
+				fi.write("\t")
+				fi.close()
+				break
+			print stdout
+			print "\tMemory size: %s" % st[0]
+			print "\tMFLOPS     : %s" % st[1]
+			print "\t", "Correct" if (st[2] == "0") else "Incorrect (error %s)" % st[2]
+			
+			#proto = ("%s\t" * 8) + "\n"
+			#print proto % ("Impl.","m","n","k","Exp.(kB)", "Mem.(kB)", "MFLOPs", "Correct")
+			#res = proto % (test,ns[i],ns[i],ns[i],memory_approx[i],st[0],st[1],st[2])
+			#print res
+			res = "\t" + st[1]
+			fi = open(filename,"a+")
+			fi.write(res)
+			fi.close()
+		fi = open(filename,"a+")
+		fi.write("\n")
+		fi.close()
+
+if __name__ == "__main__":
+	main()
