123b21f8c626ea358149ac840c309e5cbe6c9a5a
Alexander Birke <parahunter@gmail.com>
2013-01-10 13:02:35 +0100

    added first version of flag test

---
+++ assignment_1/flags_job_starter.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#PBS -N flagtest
+#PBS -q hpc
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=00:10:00
+date 
+
+
+#cd $PBS_O_WORKDIR
+
+python ./test-cpu-flags.py
+
+date
+exit 0
---
+++ assignment_1/test-cpu-flags.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+import re
+import math
+from subprocess import Popen, PIPE, call
+import datetime
+
+memory_approx =   [(x) for x in [64, 128]] #, 4096, 8192, 16384]] 
+#memory_approx =   [(x) for x in [16, 32, 64, 128, 256 , 512]] #, 4096, 8192, 16384]] 
+#memory_approx =   [(x) for x in [4, 8, 16, 32, 64, 128, 256 , 512, 1024, 2048]] #, 4096, 8192, 16384]] 
+#implementation = ["nat","nmk","nkm","knm","kmn","mnk","mkn"] # "blk"] (when ready)
+implementation = ["nat","nmk","nkm","knm","kmn","mnk","mkn"] # "blk"] (when ready)
+times = 1
+
+compiler_options = ["-fast-restrict"]
+
+def main():
+	for option in compiler_options:
+		call("make clean", shell=True)
+		call("make DRY=-xdryrun | tee > makeinfo.txt", shell=True)
+		call("make FLAGS=" + option , shell=True)
+	
+		ns = [(int(math.ceil(math.sqrt(x*1024/(3*8))))) for x in memory_approx]
+		print ns
+		now = datetime.datetime.now()
+		filename = "results_%s.dat" % (now.strftime("%Y-%m-%d_%H-%M"))
+		print "Tests running..."
+		for i in range(len(memory_approx)):
+			fi = open(filename,"a+")
+			fi.write(str(memory_approx[i]))
+			fi.close()
+			for test in implementation:
+				print "Testing matmult_%s(), %d time(s), %d kB memory." % (test,times, memory_approx[i])
+				process = Popen(("./matmult_f.studio %s %d %d %d" % (test,ns[i],ns[i],ns[i]+1)).split() ,stdout= PIPE,bufsize=-1) 
+				(stdout,stderr) = process.communicate()
+				process.wait()
+				st = re.sub(' +',' ', stdout).split(" ")[1:]
+				print "Result: " 
+				if stdout == "":
+					fi = open(filename,"a+")
+					fi.write("\t")
+					fi.close()
+					break
+				print stdout
+				print "\tMemory size: %s" % st[0]
+				print "\tMFLOPS     : %s" % st[1]
+				print "\t", "Correct" if (st[2] == "0") else "Incorrect (error %s)" % st[2]
+
+				res = "\t" + st[1]
+				fi = open(filename,"a+")
+				fi.write(res)
+				fi.close()
+			fi = open(filename,"a+")
+			fi.write("\n")
+			fi.close()
+		call("rm plotData.dat", shell=True)
+		call(("cp %s plotData.dat" % filename), shell=True)
+		call("gnuplot \"./flag-test/gnuplot-cpu-flags" + option + ".gp\"", shell=True)
+		call("lscpu > cpuinfo.txt", shell=True)
+if __name__ == "__main__":
+	main()
