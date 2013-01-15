8d8c7f7f233435e7e0fe304e14f43d0c0069555a
Alexander Birke <parahunter@gmail.com>
2013-01-15 16:12:11 +0100

    possion.h now with f

--- assignment2/possion.h
+++ assignment2/possion.h
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 const double wallVal = 20.0;
+const double radiatorVal = 200.0;
 
 double* createMat(int n)
 {
@@ -23,6 +24,20 @@ double* createMat(int n)
 	return result;
 }
 
+int c(double a, int n)
+{
+	return (int)( 0.5 * (double)n + 0.5 * a * (double) n );
+}
 
+double f(int i, int j, int n)
+{
+	int realSize = n + 2;	
+
+	if(c(0,n) < i && i <= c(1.0/3.0,n)  && c(-2.0/3.0,n) < j && j <= c(-1.0/3.0,n) )
+		return radiatorVal;
+	else
+		return 0;
+
+}
 
 
