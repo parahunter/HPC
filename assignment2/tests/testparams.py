1c0938ff9191fce3cf7c7d031c48971a726134bf
Alessandro Dal Corso <s120929@n-62-24-9.(none)>
2013-01-15 20:07:31 +0100

    Test scripts + commented line in makefile until Jacobi ready

---
+++ assignment2/tests/testparams.py
@@ -0,0 +1,31 @@
+ms = [10**x for x in range(1,5)]
+thresolds = [10**(-x) for x in range(1,5)]
+Ns = [2**x for x in range(0,11)]
+
+N_fixed = 1024
+m_fixed = 100
+thresold_fixed = 0.001
+#programNames = {"jacobi" : "../../jacobiSequential/jacobiSequential.exe", "gauss" : "../../gaussSequential/gaussSequential.exe"}
+programNames = {"gauss" : "./gaussSequential.exe"}
+
+params = {"fixed_iterations" : "i", "fixed_thresold" : "e"}
+unuseful_lines = 2
+
+
+def write_row_two(filename,x,y1,y2):
+	fi = open(filename,"a+")
+	fi.write("%s\t%s\t%s\n" % (x ,y1, y2))
+	fi.close()
+
+def write_row_one(filename,x,y):
+	fi = open(filename,"a+")
+	fi.write("%s\t%s\n" % (x ,y))
+	fi.close()
+
+def parseData(out):
+	outdata = []
+	for t in out.split("\n")[unuseful_lines:]:
+		if len(t.split("\t")) > 1:
+			outdata.append(float(t.split("\t")[1]))
+	return outdata[0], outdata[1], outdata[2], outdata[3]
+	
