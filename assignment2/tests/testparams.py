7aabf74107d6f84773f5fc99fdbbdfa4514ba540
Alessandro Dal Corso <s120929@n-62-24-4.(none)>
2013-01-16 13:15:40 +0100

    more tests

--- assignment2/tests/testparams.py
+++ assignment2/tests/testparams.py
@@ -1,8 +1,8 @@
-ms = [10**x for x in range(1,5)]
+ms = [100*x for x in range(1,7)]
 thresolds = [10**(-x) for x in range(1,5)]
 Ns = [2**x for x in range(0,11)]
 
-N_fixed = 1024
+N_fixed = 512
 m_fixed = 100
 thresold_fixed = 0.001
 #programNames = {"jacobi" : "../../jacobiSequential/jacobiSequential.exe", "gauss" : "../../gaussSequential/gaussSequential.exe"}
